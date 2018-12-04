3795ad50e8aad40d4ce564cbb474644b597b3e38
andrewnemeth <andrewnemeth@optonline.net>
2018-12-04 15:14:12 -0500

    Now asked for confirmation when deleting participants

--- src/views/UserProfile/UserProfile.jsx
+++ src/views/UserProfile/UserProfile.jsx
@@ -22,9 +22,8 @@ import CardAvatar from "components/Card/CardAvatar.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 import CardFooter from "components/Card/CardFooter.jsx";
 import firebase from 'db.js';
-
-import avatar from "assets/img/faces/marc.jpg";
-
+import {confirmAlert} from 'react-confirm-alert'
+import 'react-confirm-alert/src/react-confirm-alert.css'
 const styles = {
   cardCategoryWhite: {
     color: "rgba(255,255,255,.62)",
@@ -108,6 +107,7 @@ class UserProfile extends React.Component {
                 participants: newState
             });
         });
+
     }
 
     makeID(length){
@@ -154,6 +154,23 @@ class UserProfile extends React.Component {
         // firebase.database().ref('staticParticipantInfo').set({[participant.key] : participant})
     }
 
+    deleteParticipant = (key) => {
+        confirmAlert({
+            title: 'Confirm Deletion',
+            message: 'The participant will be permanently deleted',
+            buttons: [
+                {
+                    label: 'Delete',
+                    onClick: () => firebase.database().ref('staticParticipantInfo').child(key).remove()
+                },
+                {
+                    label: 'Cancel',
+                    onClick: () => {}
+                }
+            ]
+        })
+    };
+
     render(){
 
         const { classes } = this.props;
@@ -163,6 +180,7 @@ class UserProfile extends React.Component {
                     <GridItem xs={12} sm={12} md={8}>
                         <Button color="primary" onClick={this.newParticipant}>New Participant</Button>
                     </GridItem>
+
                     {this.state.participants.map((participant, i) => {
                         return (<GridItem xs={12} sm={8} md={8}>
                             <Card>
@@ -257,11 +275,9 @@ class UserProfile extends React.Component {
                                                   className={classes.textField}
                                                   margin="normal"
                                               />
-                                          </GridItem>
-                                    </GridContainer>
-                                    <GridContainer>
-                                        <GridItem xs={12} sm={12} md={5}>
+
                                             <TextField
+                                                style={styles.formElement}
                                                 id="standard-name"
                                                 label="Group"
                                                 value={participant.groups}
@@ -269,8 +285,6 @@ class UserProfile extends React.Component {
                                                 onChange={(e)=>this.setState({participants: update(this.state.participants, {[i]: {groups: {$set: e.target.value}}})})}
                                                 margin="normal"
                                             />
-                                        </GridItem>
-                                    </GridContainer>
                                       </div>
 
                                 </CardBody>
@@ -280,35 +294,15 @@ class UserProfile extends React.Component {
                                             <Button  margin="normal" color="primary" onClick={()=>{this.updateParticipant(participant)}}>Update Participant</Button>
                                         </GridItem>
                                         <GridItem xs={12} sm={12} md={6}>
-                                            <Button   margin="normal" onClick={()=>{firebase.database().ref('staticParticipantInfo').child(participant.key).remove()}} color="warning">Delete Participant</Button>
+                                            <Button margin="normal" onClick={()=>{this.deleteParticipant(participant.key)}} color="warning">Delete Participant</Button>
                                         </GridItem>
+
                                     </GridContainer>
                                 </CardFooter>
 
                             </Card>
                         </GridItem>
                     )})}
-                    {/*<GridItem xs={12} sm={12} md={4}>*/}
-                    {/*<Card profile>*/}
-                    {/*<CardAvatar profile>*/}
-                    {/*<a href="#pablo" onClick={e => e.preventDefault()}>*/}
-                    {/*<img src={avatar} alt="..." />*/}
-                    {/*</a>*/}
-                    {/*</CardAvatar>*/}
-                    {/*<CardBody profile>*/}
-                    {/*<h6 className={classes.cardCategory}>CEO / CO-FOUNDER</h6>*/}
-                    {/*<h4 className={classes.cardTitle}>Alec Thompson</h4>*/}
-                    {/*<p className={classes.description}>*/}
-                    {/*Don't be scared of the truth because we need to restart the*/}
-                    {/*human foundation in truth And I love you like Kanye loves Kanye*/}
-                    {/*I love Rick Owens’ bed design but the back is...*/}
-                    {/*</p>*/}
-                    {/*<Button color="primary" round>*/}
-                    {/*Follow*/}
-                    {/*</Button>*/}
-                    {/*</CardBody>*/}
-                    {/*</Card>*/}
-                    {/*</GridItem>*/}
                 </GridContainer>
             </div>
         );
